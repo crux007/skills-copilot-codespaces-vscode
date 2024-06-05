a7275dde18e33f950718fa102ceb11a41457bcd9
Victor Onuorah <53112571+crux007@users.noreply.github.com>
2024-06-05 00:53:05 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,7 @@
+function skillsMember() {
+  // console.log("skillsMember");
+  const member = document.querySelector(".member");
+  member.classList.add("skills");
+  member.classList.remove("about");
+  member.classList.remove("contact");
+}
